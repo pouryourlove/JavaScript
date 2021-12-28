39fef5034f05db9922b07ae73634c6e1f26242fb
pouryourlove <lqu134@naver.com>
2021-12-29 03:25:02 +0900

    template literal

---
+++ starter/CodingChallenge.js
@@ -0,0 +1,36 @@
+// Mark and John are trying to compare their BMI (Body Mass Index), which is
+// calculated using the formula:
+// BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+// and height in meter).
+// Your tasks:
+// 1. Store Mark's and John's mass and height in variables
+// let marksMass =  78;
+// let marksHeight = 1.69;
+// let johnsMass = 92;
+// let johnsHeight = 1.95;
+// // 2. Calculate both their BMIs using the formula (you can even implement both
+// // versions)
+// console.log(marksMass/marksHeight **2)
+// console.log(johnsMass/johnsHeight ** 2)
+// // 3. Create a Boolean variable 'markHigherBMI' containing information about
+// // whether Mark has a higher BMI than John.
+// const markBmi = marksMass/marksHeight **2;
+// const johnBmi = johnsMass/johnsHeight ** 2;
+// const markHigherBMI = markBmi > johnBmi;
+// console.log(markHigherBMI)
+// Test data:
+// § Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+// m tall.
+// § Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+// m tall.
+// GOOD LUCK �
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(BMIMark, BMIJohn, markHigherBMI);
--- starter/script.js
+++ starter/script.js
@@ -25,26 +25,107 @@
 // console.log(MyFirstJob);
 
 
-let javascriptIsFun = true;
-console.log(javascriptIsFun);
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
 
-// console.log(typeof true);
-console.log(typeof javascriptIsFun );
-// console.log(typeof 23);
-// console.log(typeof 'Jonas');
+// // console.log(typeof true);
+// console.log(typeof javascriptIsFun );
+// // console.log(typeof 23);
+// // console.log(typeof 'Jonas');
 
-javascriptIsFun = 'YES!';
-console.log(typeof javascriptIsFun );
+// javascriptIsFun = 'YES!';
+// console.log(typeof javascriptIsFun );
 
-let year;
-console.log(year);
-console.log(typeof year);
+// let year;
+// console.log(year);
+// console.log(typeof year);
 
-year = 1991;
-console.log(year);
-console.log(typeof year);
+// year = 1991;
+// console.log(year);
+// console.log(typeof year);
 
-console.log(typeof null);
+// console.log(typeof null);
+
+// mutate the variable,reassign the variable is possible
+
+// let age = 30;
+// age = 31;
+
+// *value cannot be changed*
+// *immutable variable*
+// const birthYear = 1991;
+// birthYear = 1990;
+
+// const job;
+
+// var job = 'programmer';
+// job = 'teacher'
+
+// lastName = 'Lee';
+// console.log(lastName);
+
+// Math operators
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
+
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3)
+// // 2 ** 3 means 2 to the power of 3 = 2 * 2 *2 
+
+// const firstName = 'Jonas';
+// const lastName = 'Lee';
+// console.log(firstName + ' ' +  lastName);
+
+// // Assignment operators
+// let x = 10 + 5; // 15
+// x += 10; // x = x + 10 = 25
+// x *= 4 // x = x *4 = 100
+// x++; // x = x + 1 
+// x--; // x = x -1 
+// x--;
+
+// console.log(x);
+
+// //comparison operators
+// console.log(ageJonas > ageSarah); // >, < , >= , <=
+// console.log(ageSarah >= 18); 
+
+// const isFullAge = ageSarah >= 18
+
+// console.log(now - 1991 > now - 2018);
+
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+
+// console.log(now - 1991 > now - 2018);
 
 
+// let x,y;
+// x = y = 25-10-5;
+// console.log(x,y);
 
+// const averageAge = (ageJonas + ageSarah) /2
+// console.log(ageJonas, ageSarah, averageAge);
+
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+
+const jonas = "I'm" + firstName + ', a' + (year - birthYear) + ' years old ' + job + '!';
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}`;
+console.log(jonasNew);
+
+console.log(`just a regular string...`);
+
+console.log('String with \n\
+multiple \n\
+lines');
+
+console.log(`String
+multiple
+lines`);
