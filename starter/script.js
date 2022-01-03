42159d09b02ab99d1ec6b3818e69a7e60d6ce28c
pouryourlove <lqu134@naver.com>
2022-01-04 00:47:44 +0900

    function added

--- starter/script.js
+++ starter/script.js
@@ -1,11 +1,34 @@
 'use strict';
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log('I can drive:D');
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log('I can drive:D');
 
-const interface = 'Audio';
-const private = 543;
-const if = 23;
+// const interface = 'Audio';
+// const private = 543;
+// const if = 23;
+
+function logger() {
+    console.log('My name is Jonas');
+}
+// calling,running,invoking function
+logger();
+logger();
+logger();
+
+function fruitsProcessor(apples, oranges){
+   
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const applejuice = fruitsProcessor(5, 0); //arguments
+console.log(applejuice);
+console.log(fruitsProcessor(5, 0));
+
+const appleOrangeJuice = fruitsProcessor(2,4);
+console.log(appleOrangeJuice);
+
+const num = Number('23');
