891d5a84d7c8625e62f03cf731dabcc5489a4ac1
pouryourlove <lqu134@naver.com>
2022-01-12 00:18:03 +0900

    while loop added

--- starter/script.js
+++ starter/script.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 // let hasDriversLicense = false;
 // const passTest = true;
@@ -247,18 +247,116 @@
 
 // console.log(jonas.getSummary());
 
-// console.log('Lifting weights repetition 1');
-// console.log('Lifting weights repetition 2');
-// console.log('Lifting weights repetition 3');
-// console.log('Lifting weights repetition 4');
-// console.log('Lifting weights repetition 5');
-// console.log('Lifting weights repetition 6');
-// console.log('Lifting weights repetition 7');
-// console.log('Lifting weights repetition 8');
-// console.log('Lifting weights repetition 9');
-// console.log('Lifting weights repetition 10');
+// console.log("Lifting weights repetition 1");
+// console.log("Lifting weights repetition 2");
+// console.log("Lifting weights repetition 3");
+// console.log("Lifting weights repetition 4");
+// console.log("Lifting weights repetition 5");
+// console.log("Lifting weights repetition 6");
+// console.log("Lifting weights repetition 7");
+// console.log("Lifting weights repetition 8");
+// console.log("Lifting weights repetition 9");
+// console.log("Lifting weights repetition 10");
 
-//for loop keeps running while condition is TRUE
-for (let rep = 1; rep <= 10; rep = rep++) {
-  console.log("Lifting weights repetition 1");
-}
+// for loop keeps running while condition is TRUE
+// for (let rep = 1; rep <= 10; rep++) {
+//   console.log(`Lifting weights repetition ${rep}`);
+// }
+
+// const jonas = [
+//   "Jonas",
+//   "Lee",
+//   2037 - 1991,
+//   "teacher",
+//   ["Michael", "Peter", "Steven"],
+//   true,
+// ];
+
+// const types = [];
+
+// console.log(jonas[0])
+// console.log(jonas[1])
+// console.log(jonas[2])
+// console.log(jonas[3])
+// console.log(jonas[4])
+// jonas[5] does NOT exist
+
+// for (let i = 0; i < jonas.length; i++) {
+//   //Reading from jonas array
+//   console.log(jonas[i], typeof jonas[i]);
+
+//   //Filling types array
+//   //   types[i] = typeof jonas[i];
+//   types.push(typeof jonas[i]);
+// }
+
+// console.log(types);
+
+// const years = [1991, 2007, 1969, 2020];
+// const ages = [];
+
+// for (let i = 0; i < years.length; i++) {
+//   ages.push(2037 - years[i]);
+// }
+// console.log(ages);
+
+// //continue and break
+// console.log("---ONLY STRINGS ---");
+// for (let i = 0; i < jonas.length; i++) {
+//   if (typeof jonas[i] !== "string") continue;
+
+//   console.log(jonas[i], typeof jonas[i]);
+// }
+
+// console.log("---BREAK WITH NUMBER ---");
+// for (let i = 0; i < jonas.length; i++) {
+//   if (typeof jonas[i] === "number") break;
+
+//   console.log(jonas[i], typeof jonas[i]);
+// }
+
+// const jonas = [
+//   "Jonas",
+//   "Lee",
+//   2037 - 1991,
+//   "teacher",
+//   ["Michael", "Peter", "Steven"],
+//   true,
+// ];
+
+// 0,1, ....4
+// 4,3, ....0
+
+// for (let i = jonas.length - 1; i >= 0; i--) {
+//   console.log(i, jonas[i]);
+// }
+
+// for (let exercise = 1; exercise < 4; exercise++) {
+//   console.log(`--------Starting exercise ${exercise}`);
+
+//   for (let rep = 1; rep < 6; rep++) {
+//     console.log(` Exercise ${exercise} : Lifting weight reptition ${rep}`);
+//   }
+// }
+
+// for (let rep = 1; rep <= 10; rep++) {
+//   console.log(`Lifting weights repetition ${rep}`);
+// }
+
+// let rep = 1;
+// while (rep <= 10) {
+//   console.log(`WHILE:Lifting weights repetition ${rep}`);
+//   rep++;
+// }
+
+// let dice = Math.trunc(Math.random() * 6) + 1;
+// console.log(dice);
+
+// while (dice !== 6) {
+//   console.log(`You rolled a ${dice}`);
+//   dice = Math.trunc(Math.random() * 6) + 1;
+//   if (dice === 6) console.log("Loop is about to end...");
+// }
+
+const calcAge = birthYear => 2037 - birthYear;
+console.log(calcAge(1991));
