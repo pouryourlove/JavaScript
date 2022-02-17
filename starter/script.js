f83f9a8fbd237e0b9a6bb4222d82b9698e7f2f82
pouryourlove <lqu134@naver.com>
2022-02-17 15:34:38 +0900

    call, apply, bind method

--- starter/script.js
+++ starter/script.js
@@ -21,33 +21,33 @@
 // console.log(flight);
 // console.log(juyeon);
 
-const oneWord = function (str) {
-  return str.replace(/ /g, '').toLowerCase();
-};
+// const oneWord = function (str) {
+//   return str.replace(/ /g, '').toLowerCase();
+// };
 
-const upperFirstWord = function (str) {
-  const [first, ...others] = str.split(' ');
-  return [first.toUpperCase(), ...others].join(' ');
-};
+// const upperFirstWord = function (str) {
+//   const [first, ...others] = str.split(' ');
+//   return [first.toUpperCase(), ...others].join(' ');
+// };
 
-//Higher-order function
-const transformer = function (str, fn) {
-  console.log(`Original string: ${str}`);
-  console.log(`transformed string: ${fn(str)}`);
+// //Higher-order function
+// const transformer = function (str, fn) {
+//   console.log(`Original string: ${str}`);
+//   console.log(`transformed string: ${fn(str)}`);
 
-  console.log(`Transformed by: ${fn.name}`);
-};
+//   console.log(`Transformed by: ${fn.name}`);
+// };
 
-transformer('JavaScript is the best!', upperFirstWord);
-transformer('JavaScript is the best!', oneWord);
+// transformer('JavaScript is the best!', upperFirstWord);
+// transformer('JavaScript is the best!', oneWord);
 
-//JS uses callbacks all the time
-const high5 = function () {
-  console.log('❤');
-};
-document.body.addEventListener('click', high5);
+// //JS uses callbacks all the time
+// const high5 = function () {
+//   console.log('❤');
+// };
+// document.body.addEventListener('click', high5);
 
-['juyeon', 'Stephnaie', 'Maisy'].forEach(high5);
+// ['juyeon', 'Stephnaie', 'Maisy'].forEach(high5);
 
 //Is the same as doing...
 // const flightNum = flight;
@@ -59,3 +59,97 @@ document.body.addEventListener('click', high5);
 
 // newPassport(juyeon);
 // checkIn(flight, juyeon);
+
+// const greet = function (greeting) {
+//   return function (name) {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
+
+//Challenge
+// const greet = greeting => name => console.log(`${greeting} ${name}`);
+
+// const greeterHey = greet('Hey');
+// greeterHey('Juyeon');
+// greeterHey('Maisy');
+
+// greet('Hello')('Juyeon');
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, 'Juyeon Lee');
+lufthansa.book(635, 'Maisy Dodd');
+// console.log(lufthansa);
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+// //Does NOT work
+// // book(23, 'Juyeon Lee');
+
+//Call method
+book.call(eurowings, 23, 'Stephanie');
+
+book.call(lufthansa, 239, 'Gaelle lee');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 583, 'Jacob cooper');
+console.log(swiss);
+
+//Apply method
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+book.call(swiss, ...flightData);
+
+//Bind method
+
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+bookEW(23, 'Zzong Lee');
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Juyeon Lee');
+bookEW23('Maisy dodd');
+
+//With Event Listners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+
+  this.planes++;
+  console.log(this.planes);
+};
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVat = addTax.bind(null);
