b4aff5b783d7f1ae6b8a4136c152071442849d7e
pouryourlove <lqu134@naver.com>
2022-03-22 22:09:09 +0900

    block scope added

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
 // ==============
 // FUNCTION SCOPE
 // ==============
-let totalEggs = 0;
-function collectEggs() {
-  totalEggs = 6;
-}
-console.log(totalEggs);
-collectEggs();
-console.log(totalEggs);
+// let totalEggs = 0;
+// function collectEggs() {
+//   totalEggs = 6;
+// }
+// console.log(totalEggs);
+// collectEggs();
+// console.log(totalEggs);
 
-const bird = "Scarlet Macaw";
-function birdWatch() {
-  const bird = "Great Blue Heron";
-  console.log(bird);
+// const bird = "Scarlet Macaw";
+// function birdWatch() {
+//   const bird = "Great Blue Heron";
+//   console.log(bird);
+// }
+// birdWatch();
+
+// ==============
+// BLOCK SCOPE
+// ==============
+let radius = 8;
+if (radius > 0) {
+  const PI = 3.14159;
+  let msg = "HIII!";
 }
-birdWatch();
+console.log(radius);
+console.log(msg);
+
+for (let i = 0; i < 5; i++) {
+  let msg = "ASKLDJAKLSJD";
+  console.log(msg);
+}
+console.log(msg);
+console.log(i);
