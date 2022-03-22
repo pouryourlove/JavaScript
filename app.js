f2fcb3cf220ebfd945dfe096ec5a131debd2bc3b
pouryourlove <lqu134@naver.com>
2022-03-22 22:03:44 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// ==============
+// FUNCTION SCOPE
+// ==============
+let totalEggs = 0;
+function collectEggs() {
+  totalEggs = 6;
+}
+console.log(totalEggs);
+collectEggs();
+console.log(totalEggs);
+
+const bird = "Scarlet Macaw";
+function birdWatch() {
+  const bird = "Great Blue Heron";
+  console.log(bird);
+}
+birdWatch();
