1733b0c4c79ce6e1ee3c7b244048a6c15a4bb323
pouryourlove <lqu134@naver.com>
2022-03-25 21:07:02 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+// function sum(num1, num2) {
+//   return num1 + num2;
+// }
+
+// const sum = (num1, num2) => {
+//   return num1 + num2;
+// };
+
+// const sum = (num1, num2) => num1 + num2;
+
+// sum(40, 2);
+// sum(42, 0);
+// console.log(sum(42, 0));
+
+// function stringLength(str) {
+//   console.log(`the length of "${str}" is:`, str.length);
+// }
+
+// let longestCityNameInTheWorld =
+//   "Taumatawhakatangihangakoauauotamateaturipukakapikimaungahoronukupokaiwhenuakitanatahu";
+
+// stringLength(longestCityNameInTheWorld);
+
+// function stringLength(str) {
+//   console.log(`the length of "${str}" is:`, str.length);
+// }
+
+const stringLength = (str) =>
+  console.log(`the length of "${str}" is:`, str.length);
+
+let longestCityNameInTheWorld =
+  "Taumatawhakatangihangakoauauotamateaturipukakapikimaungahoronukupokaiwhenuakitanatahu";
+stringLength(longestCityNameInTheWorld);
