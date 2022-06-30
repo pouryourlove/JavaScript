f7ee1e2bd017c6f2aa451be7eabf8d4a65a0b0e2
Juyeon Lee <lqu134@naver.com>
2022-06-30 23:58:35 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// setTimeout(() => {
+//   document.body.style.backgroundColor = "red";
+//   setTimeout(() => {
+//     document.body.style.backgroundColor = "yellow";
+//     setTimeout(() => {
+//       document.body.style.backgroundColor = "brown";
+//       setTimeout(() => {
+//         document.body.style.backgroundColor = "green";
+//         setTimeout(() => {
+//           document.body.style.backgroundColor = "blue";
+//         }, 1000);
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+const delayedColorChange = (newColor, delay, doNext) => {
+  setTimeout(() => {
+    document.body.style.backgroundColor = newColor;
+    doNext();
+  }, delay);
+};
+
+delayedColorChange("red", 1000, () => {
+  delayedColorChange("orange", 1000, () => {
+    delayedColorChange("yellow", 1000, () => {
+      delayedColorChange("green", 1000);
+    });
+  });
+});
