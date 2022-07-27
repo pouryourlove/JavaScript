646cbc1ae04471b68636fb8bdb3abc52737d4885
Juyeon Lee <lqu134@naver.com>
2022-07-27 16:53:31 +0900

    Prototype and proto

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ const Person = function (firstName, birthYear) {
   this.firstName = firstName;
   this.birthYear = birthYear;
 
-  this.calAge = function () {
-    console.log(2037 - this.birthYear);
-  };
+  //Never do this!! We need to use Prototypes and prototypal inheritance
+  // this.calAge = function () {
+  //   console.log(2037 - this.birthYear);
+  // };
 };
 
-//Never do this!! We need to use Prototypes and prototypal inheritance
-// const jonas = new Person('Jonas', 1991);
-// console.log(jonas);
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
 
 // 1.New {} is created
-// 2.function is called, this = {}
-// 3.{} linked to prototype
-// 4.function automatically return {}
+// 2.function is called, this = {} this keyword is set to the new empty object as no.1 this keyword point to this object
+// 3.{} linked to prototype (new object linked to prototype)
+// 4.function automatically return {} (no.1 object)
 
 const matilda = new Person('Matilda', 2017);
 const jack = new Person('jack', 1975);
@@ -26,3 +26,31 @@ const jay = 'jay';
 
 console.log(jonas instanceof Person);
 console.log(jay instanceof Person);
+
+// Prototypes
+console.log(Person.prototype);
+
+Person.prototype.calAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calAge();
+matilda.calAge();
+jack.calAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+//Person.prototype is not a prototype of a Person. It is a prototype of linked Objects
+
+Person.prototype.species = 'Homo Spiens';
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
+
+//Second one says false because this property is not really inside of the jonas object. it only has access to it because of prototype of a person
