fd3af4f7b5d6f7d63e649400d65e70f66d79ce79
Juyeon Lee <lqu134@naver.com>
2022-08-10 23:28:59 +0900

    ES6 classes

--- script.js
+++ script.js
@@ -1,76 +1,111 @@
 'use strict';
 
-const Person = function (firstName, birthYear) {
-  this.firstName = firstName;
-  this.birthYear = birthYear;
+// const Person = function (firstName, birthYear) {
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
 
-  //Never do this!! We need to use Prototypes and prototypal inheritance
-  // this.calAge = function () {
-  //   console.log(2037 - this.birthYear);
-  // };
-};
+//   //Never do this!! We need to use Prototypes and prototypal inheritance
+//   // this.calAge = function () {
+//   //   console.log(2037 - this.birthYear);
+//   // };
+// };
 
-const jonas = new Person('Jonas', 1991);
-console.log(jonas);
+// const jonas = new Person('Jonas', 1991);
+// console.log(jonas);
 
-// 1.New {} is created
-// 2.function is called, this = {} this keyword is set to the new empty object as no.1 this keyword point to this object
-// 3.{} linked to prototype (new object linked to prototype)
-// 4.function automatically return {} (no.1 object)
+// // 1.New {} is created
+// // 2.function is called, this = {} this keyword is set to the new empty object as no.1 this keyword point to this object
+// // 3.{} linked to prototype (new object linked to prototype)
+// // 4.function automatically return {} (no.1 object)
 
-const matilda = new Person('Matilda', 2017);
-const jack = new Person('jack', 1975);
-console.log(matilda, jack);
+// const matilda = new Person('Matilda', 2017);
+// const jack = new Person('jack', 1975);
+// console.log(matilda, jack);
 
-const jay = 'jay';
+// const jay = 'jay';
 
-console.log(jonas instanceof Person);
-console.log(jay instanceof Person);
+// console.log(jonas instanceof Person);
+// console.log(jay instanceof Person);
 
-// Prototypes
-console.log(Person.prototype);
+// // Prototypes
+// console.log(Person.prototype);
 
-Person.prototype.calAge = function () {
-  console.log(2037 - this.birthYear);
-};
+// Person.prototype.calAge = function () {
+//   console.log(2037 - this.birthYear);
+// };
 
-jonas.calAge();
-matilda.calAge();
-jack.calAge();
+// jonas.calAge();
+// matilda.calAge();
+// jack.calAge();
 
-console.log(jonas.__proto__);
-console.log(jonas.__proto__ === Person.prototype);
+// console.log(jonas.__proto__);
+// console.log(jonas.__proto__ === Person.prototype);
 
-console.log(Person.prototype.isPrototypeOf(jonas));
-console.log(Person.prototype.isPrototypeOf(matilda));
-console.log(Person.prototype.isPrototypeOf(Person));
+// console.log(Person.prototype.isPrototypeOf(jonas));
+// console.log(Person.prototype.isPrototypeOf(matilda));
+// console.log(Person.prototype.isPrototypeOf(Person));
 
-//Person.prototype is not a prototype of a Person. It is a prototype of linked Objects
+// //Person.prototype is not a prototype of a Person. It is a prototype of linked Objects
 
-Person.prototype.species = 'Homo Spiens';
-console.log(jonas.species, matilda.species);
+// Person.prototype.species = 'Homo Spiens';
+// console.log(jonas.species, matilda.species);
 
-console.log(jonas.hasOwnProperty('firstName'));
-console.log(jonas.hasOwnProperty('species'));
+// console.log(jonas.hasOwnProperty('firstName'));
+// console.log(jonas.hasOwnProperty('species'));
 
-//Second one says false because this property is not really inside of the jonas object. it only has access to it because of prototype of a person
+// //Second one says false because this property is not really inside of the jonas object. it only has access to it because of prototype of a person
 
-// Object.prototype (top of prototype chain)
+// // Object.prototype (top of prototype chain)
 
-console.log(jonas.__proto__);
-console.log(jonas.__proto__.__proto__);
-console.log(jonas.__proto__.__proto__.__proto__);
+// console.log(jonas.__proto__);
+// console.log(jonas.__proto__.__proto__);
+// console.log(jonas.__proto__.__proto__.__proto__);
 
-const arr = [3, 6, 6, 5, 6, 9, 9]; //new Array === []
-console.log(arr.__proto__);
-console.log(arr.__proto__ === Array.prototype);
+// const arr = [3, 6, 6, 5, 6, 9, 9]; //new Array === []
+// console.log(arr.__proto__);
+// console.log(arr.__proto__ === Array.prototype);
 
-console.log(arr.__proto__.__proto__);
+// console.log(arr.__proto__.__proto__);
 
-Array.prototype.unique = function () {
-  return [...new Set(this)];
-};
+// Array.prototype.unique = function () {
+//   return [...new Set(this)];
+// };
 
-console.log(arr.unique());
+// console.log(arr.unique());
 
-const h1 = document.querySelector('h1');
+// const h1 = document.querySelector('h1');
+
+// class expression
+// const PersonCl = class {};
+
+// class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+}
+
+const jessica = new PersonCl('Jessica', 1996);
+console.log(jessica);
+jessica.calcAge();
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+jessica.greet();
+
+// 1. Classes are NOT hoisted
+// 2. Class are first-class citizens
+// 3. Classes are executed in strict mode
