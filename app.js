be831028df4a184ff9b9f234e8d9554c9659f61c
Juyeon Lee <lqu134@naver.com>
2022-08-30 21:18:32 +0900

    loading username

--- app.js
+++ app.js
@@ -3,14 +3,29 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
   //   greeting.innerText = "Hello " + username;
-  greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  paintGreeting(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreeting(username) {
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hello ${username}`;
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  // show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  //show the greetings
+  paintGreeting(savedUsername);
+}
