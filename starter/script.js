d243eb2ca7f93669d43915729680dca9ebf46890
pouryourlove <lqu134@naver.com>
2022-11-07 19:02:54 +0900

    regular functions vs arrow functions

--- starter/script.js
+++ starter/script.js
@@ -42,33 +42,76 @@
 
 // console.log(this);
 
-const calcAge = function (birthYear) {
-  console.log(2037 - birthYear);
-  // console.log(this); //undefined
-};
-calcAge(1991);
+// const calcAge = function (birthYear) {
+//   console.log(2037 - birthYear);
+//   // console.log(this); //undefined
+// };
+// calcAge(1991);
 
-const calcAgeArrow = birthYear => {
-  console.log(2037 - birthYear);
-  // console.log(this); //window object
-};
-calcAge(1980);
+// const calcAgeArrow = birthYear => {
+//   console.log(2037 - birthYear);
+//   // console.log(this); //window object
+// };
+// calcAge(1980);
+
+// const jonas = {
+//   year: 1991,
+//   calcAge: function () {
+//     console.log(this); //jonas
+//     console.log(2037 - this.year);
+//   },
+// };
+// jonas.calcAge();
+
+// const matilda = {
+//   year: 2017,
+// };
+
+// matilda.calcAge = jonas.calcAge;
+// matilda.calcAge();
+
+// const f = jonas.calcAge;
+// f();
 
 const jonas = {
+  firstName: 'Jonas',
   year: 1991,
   calcAge: function () {
     console.log(this); //jonas
     console.log(2037 - this.year);
+
+    //Solution 1
+    // const self = this; //self or that
+    // const isMillenial = function () {
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    //   // console.log(this.year >= 1981 && this.year <= 1996);
+    // };
+    // isMillenial();
+
+    // Soulution 2
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
   },
+
+  greet: () => console.log(`Hey ${this.firstName}`),
 };
+
+jonas.greet();
 jonas.calcAge();
 
-const matilda = {
-  year: 2017,
+//argument keyword
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
 };
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
 
-matilda.calcAge = jonas.calcAge;
-matilda.calcAge();
-
-const f = jonas.calcAge;
-f();
+var addArrow = (a, b) => {
+  console.log(arguments); // Uncaught ReferenceError: arguments is not defined
+  return a + b;
+};
+addArrow(2, 5, 8);
