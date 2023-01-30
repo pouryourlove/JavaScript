16a405e0bec2eeeef3b1d97cf302a291139b9e0c
pouryourlove <lqu134@naver.com>
2023-01-30 23:29:15 +0900

    forEach

--- starter/script.js
+++ starter/script.js
@@ -71,7 +71,7 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -109,17 +109,38 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // // JOIN
 // console.log(letters.join(' - '));
 
-const arr = [23, 11, 64];
-console.log(arr[0]);
-console.log(arr.at(0));
+// const arr = [23, 11, 64];
+// console.log(arr[0]);
+// console.log(arr.at(0));
 
-// getting last array element
-console.log(arr[arr.length - 1]);
-console.log(arr.slice(-1)[0]);
-console.log(arr.at(-1));
+// // getting last array element
+// console.log(arr[arr.length - 1]);
+// console.log(arr.slice(-1)[0]);
+// console.log(arr.at(-1));
 
-console.log('juyeon'.at(0));
-console.log('juyeon'.at(-1));
+// console.log('juyeon'.at(0));
+// console.log('juyeon'.at(-1));
 
 //at method is perfect for getting the last element of the array
 //and if you want to do something called "method chaining"
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1} : You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1} : You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+//break and continue doesn't work with for each loop
+console.log('----FOREACH');
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Mov ${i + 1} : You deposited ${mov}`);
+  } else {
+    console.log(`Mov ${i + 1} : You withdrew ${Math.abs(mov)}`);
+  }
+});
