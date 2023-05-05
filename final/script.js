2724d3fc25a854bebfb68911c87bdc8afcb67212
Juyeon Lee <lqu134@naver.com>
2023-05-05 15:36:59 +0900

    coding challenge 4

--- final/script.js
+++ final/script.js
@@ -656,7 +656,6 @@ labelBalance.addEventListener('click', function () {
 ///////////////////////////////////////
 // Array Methods Practice
 
-
 // 1.
 const bankDepositSum = accounts
   .flatMap(acc => acc.movements)
@@ -747,12 +746,11 @@ GOOD LUCK 😀
 */
 
 /*
-const dogs = [
-  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
-  { weight: 8, curFood: 200, owners: ['Matilda'] },
-  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
-  { weight: 32, curFood: 340, owners: ['Michael'] },
-];
+
+
+
+
+
 
 // 1.
 dogs.forEach(dog => (dog.recFood = Math.trunc(dog.weight ** 0.75 * 28)));
