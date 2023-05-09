1585753b114f4ab827e39d69de000574f76ce1bf
Juyeon Lee <lqu134@naver.com>
2023-05-09 23:36:33 +0900

    remainder operator

--- starter/script.js
+++ starter/script.js
@@ -326,3 +326,26 @@ console.log((2.345).toFixed(2));
 //The result is 2.35 as I wrote above, it is still string.
 //if we want to change it to a number you can do this
 console.log(+(2.345).toFixed(2));
+
+console.log(5 % 2);
+console.log(5 / 2); // 5 = 2 * 2 + 1
+console.log(8 % 3);
+console.log(8 / 3); // 8 = 2 * 3 + 2
+
+console.log(6 % 2);
+console.log(6 / 2);
+
+console.log(7 % 2);
+console.log(7 / 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+    if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+    if (i % 3 === 0) row.style.backgroundColor = 'blue';
+  });
+});
