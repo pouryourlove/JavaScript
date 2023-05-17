c623ee0f813e2f8f8011ebc1b0e788377b4704ce
Juyeon Lee <lqu134@naver.com>
2023-05-17 23:31:51 +0900

    today yesterday days ago completed

--- starter/script.js
+++ starter/script.js
@@ -21,9 +21,9 @@ const account1 = {
     '2020-01-28T09:15:04.904Z',
     '2020-04-01T10:17:24.185Z',
     '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2023-05-14T17:01:17.194Z',
+    '2023-05-15T23:36:17.929Z',
+    '2023-05-16T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -83,12 +83,24 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 const formatMovementDate = function(date){
   
-  const calcDayPassed = (date1, date2) => Math.abs(date2 - date1) / (1000 * 60 * 60 * 24)
-  const day = `${date.getDate()}`.padStart(2, 0);
-  const month = `${date.getMonth() + 1}`.padStart(2, 0);
-  const year = date.getFullYear();
+  const calcDayPassed = (date1, date2) => Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24))
+  
+  const daysPassed = calcDayPassed(new Date(), date)
+  console.log(daysPassed);
 
-  return `${day}/${month}/${year}`;
+  if(daysPassed === 0) return 'Today'
+  if(daysPassed === 1) return 'Yesterday'
+  if(daysPassed <= 7) return `${daysPassed} days ago`
+  else{
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+  
+    return `${day}/${month}/${year}`;
+
+  }
+  
+ 
 
   
 
